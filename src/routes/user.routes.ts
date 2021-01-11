c897f448a90dd04733aa795ab5bb16e0d8a95803
lumilitas <lucas23121995>
2021-01-11 08:23:33 -0300

    Atualizando avatar

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,9 @@
-import { response, Router } from 'express';
+import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middleware/ensureAuthenticated';
 
@@ -32,9 +33,20 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file);
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
 
-    return response.json({ ok: true });
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 );
 
